3daa13a0085698d4a0dd6a276450a4f1baf56e08
Margustr <mmargus960@gmail.com>
2024-09-17 16:51:55 +0300

    Update ayarlar.js

--- ayarlar.js
+++ ayarlar.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: '', 
-    sahipID: '',
+    token: 'MTI4NTI4MDE1MTc0Mjk3MjA5NQ.G5BrZQ.tbL2a7VAeXU_m_VTHJTV7hyGn6cJXLBWmKxoN8', 
+    sahipID: '1258519618767818945',
   };
