ca2c5cda3ce0e8a13d4fbca5c165913ac9ce022b
Margustr <mmargus960@gmail.com>
2024-09-17 16:47:27 +0300

    Update sorular.js

--- modules/sorular.js
+++ modules/sorular.js
@@ -1,7 +1,7 @@
 module.exports = {
     soru1: 'Günde kaç saat aktif olursun?',
     soru2: 'Daha önce hiç yetkili oldun mu?',
-    soru3: 'Daha önce hangi projelerde çalıştın?',
-    soru4: 'Mikrofonun var mı?',
-    soru5: 'Bu sunucuya neler katacaksın?',
+    soru3: 'iz,org,result/slot yapabilirmisin?',
+    soru4: 'Günde kaç kere partnerlik yapabilirsin?',
+    soru5: 'Neden bizi seçtin?',
   };
